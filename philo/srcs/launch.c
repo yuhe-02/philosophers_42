2d5c0f069fcf7010442b44c37b68a7843e490597
yuhe-02 <domineter4649@gmail.com>
2024-12-19 19:57:23 +0000

    ref launch error

--- philo/srcs/launch.c
+++ philo/srcs/launch.c
@@ -6,11 +6,43 @@
 /*   By: yyamasak <yyamasak@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/19 16:26:48 by yyamasak          #+#    #+#             */
-/*   Updated: 2024/12/19 19:14:22 by yyamasak         ###   ########.fr       */
+/*   Updated: 2024/12/19 19:57:07 by yyamasak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
+
+static	int	destroy_key(t_params *params, int end, int ret)
+{
+	int	i;
+
+	i = 0;
+	while (i < end)
+	{
+		pthread_mutex_destroy(&(params->forks[i]));
+		pthread_mutex_destroy(&(params->meal_keys[i]));
+		i++;
+	}
+	pthread_mutex_destroy(&(params->print_key));
+	pthread_mutex_destroy(&(params->dead_key));
+	return (ret);
+}
+
+// static	int destroy_all(t_params *params, int end)
+// {
+// 	int	i;
+
+// 	i = 0;
+// 	while (i < end)
+// 	{
+// 		pthread_detach(params->philo[i].t_id); 
+// 		i++;
+// 	}
+// 	if (i == params->nop)
+// 		pthread_detach(params->owner.t_id);
+// 	destroy_key(params, i);
+// 	return (1);
+// }
 
 static int	all_join_thread(t_params *params)
 {
@@ -37,16 +69,17 @@ int	launch_thread(t_params *params)
 	t_owner	*owner;
 
 	i = 0;
+	owner = &(params->owner);
 	while (i < params->nop)
 	{
 		philo = &(params->philo[i]);
-		pthread_create(&(philo->t_id), NULL, start_philo, (void *)philo);
+		if (pthread_create(&(philo->t_id), NULL, start_philo, (void *)philo))
+			return (destroy_key(params, params->nop, 1));
 		philo->last_meal = ft_timeofday();
 		i++;
 	}
-	owner = &(params->owner);
-	pthread_create(&(owner->t_id), NULL, start_owner, (void *)(owner));
-	i = 0;
+	if (pthread_create(&(owner->t_id), NULL, start_owner, (void *)(owner)))
+		return (destroy_key(params, params->nop, 1));
 	all_join_thread(params);
-	return (0);
+	return (destroy_key(params, params->nop, 0));
 }
